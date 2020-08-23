f1f43801429d9c242f6f4df798436f2e5c89b7ce
minefarmer <pgoldfarm@yahoo.com>
2020-08-22 21:55:50 -0500

    Good Night!

---
+++ ClassWork/ExpertGuide/Variables.py
@@ -0,0 +1,19 @@
+# take two numbers
+Number1 = 10
+Number2 = 29
+
+# add item
+Sum = Number1 + Number2
+print('Sum: ',Sum)  # Sum:  39
+
+# subtract
+Diff = Number1 - Number2
+print('Difference:',Diff)  # Difference: -19
+
+Number3 = 5
+
+# multiply sum calculated above with third number
+Mul = Sum + Number3
+print('Multiplication:',Mul);  # Multiplication: 44
+
+print('python is an interesting language')  # python is an interesting language
