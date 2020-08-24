dd79fd242cbf0d9a5a863f58f4b5afc230c9b735
minefarmer <pgoldfarm@yahoo.com>
2020-08-24 10:41:46 -0500

    back to linux

--- ClassWork/ExpertGuide/IfElse.py
+++ ClassWork/ExpertGuide/IfElse.py
@@ -8,9 +8,9 @@
 
 
 #  Second video
-a=int(input("Enter first number "))  
-b=int(input("Enter second number "))  
-c=int(input("enter third number "))  
+# a=int(input("Enter first number "))  
+# b=int(input("Enter second number "))  
+# c=int(input("enter third number "))  
 # if a>b>c:
 #     print('a is the greatest', a)  # 4
 # elif b>(a>c):
@@ -19,10 +19,29 @@ c=int(input("enter third number "))
 #     print('c is the greatest', c)  # 6
 #                                    # b is the greatest 9
 
-if a>b and b>c:
-    print('greatest',a)  # Enter first number 6
-elif b>a and b>c:
-    print('greatest',b)  # Enter second number 3
-else:
-    print('greatest',c)  # enter third number 9
-                         # greatest 9
+# if a>b and b>c:
+#     print('greatest',a)  # Enter first number 6
+# elif b>a and b>c:
+#     print('greatest',b)  # Enter second number 3
+# else:
+#     print('greatest',c)  # enter third number 9
+#                          # greatest 9
+                         
+                         
+
+
+# Third video
+while True:
+    a=int(input("Enter first no"))
+    b= int(input("Enter second no"))
+    c= int(input("Enter third no"))
+    if a>b and a>c:
+        print("a is greatest ",a)
+    elif b>a and b > c:
+        print('b is greatest', b)
+    else:
+        print('c is greatest',c)
+    reply=input('enter stop to exit')
+    if reply =='stop': break
+        
+        
