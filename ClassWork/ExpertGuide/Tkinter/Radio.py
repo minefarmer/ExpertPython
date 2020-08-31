2b78d568f81e73b195fb61d7db372eacac041610
minefarmer <pgoldfarm@yahoo.com>
2020-08-30 21:06:47 -0500

    Good night from Linux

--- ClassWork/ExpertGuide/Tkinter/Radio.py
+++ ClassWork/ExpertGuide/Tkinter/Radio.py
@@ -1,1 +1,22 @@
 from tkinter import *
+
+def sel():
+    selection = "You selected the option " + str(var.get())
+    l.config(text = selection)
+
+root = Tk()
+var = IntVar()
+R1 = Radiobutton(root, text="Option 1", variable=var, value=1, command=sel)
+R1.pack( anchor = N)
+
+R2 = Radiobutton(root, text="Option 2", variable=var, value=2, command=sel)
+R2.pack( anchor = W )
+
+R3 = Radiobutton(root, text="Option 3", variable=var, value=3)
+
+R3.pack( anchor = S)
+
+l = Label(root)
+l.pack()
+root.mainloop()
+
---
+++ ClassWork/ExpertGuide/Tkinter/Variable.py
@@ -0,0 +1,4 @@
+from tkinter import *
+
+def onButtonPress():
+    LabelVariable.set(8welcome)
